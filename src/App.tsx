8950e213a9f41f88ff273728752d7e6f929934c3
HamJohni <joni.pacan.wf@gmail.com>
2023-03-16 12:36:27 +0300

    form for auth

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {Preloader} from "./pages/Preloader/Preloader";
 import {Layout} from "./Layout/Layout";
 import {Home} from './pages/Home/Home'
 import {Register} from "./pages/Register/Regiser";
+import {Login} from "./pages/Login/Login";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 
         <Routes>
             <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
             <Route path='/' element={<Layout/>}>
                 <Route path='' element={<Home/>}/>
             </Route>
